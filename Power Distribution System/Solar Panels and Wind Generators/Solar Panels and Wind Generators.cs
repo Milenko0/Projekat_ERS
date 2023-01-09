57113c64943b66c1869725aa58628f9ceb7a56bc
misli112 <48420958+misli112@users.noreply.github.com>
2023-01-09 19:52:12 +0100

    Add files via upload

--- Power Distribution System/Solar Panels and Wind Generators/Solar Panels and Wind Generators.cs
+++ Power Distribution System/Solar Panels and Wind Generators/Solar Panels and Wind Generators.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Solar_Panels_and_Wind_Generators
 {
@@ -17,8 +18,8 @@ namespace Solar_Panels_and_Wind_Generators
 
         public Solar_Panels_and_Wind_Generators()
         {
-            JacinaSunca = 100;
-            JacinaVetra = 100;
+            JacinaSunca = 50;
+            JacinaVetra = 50;
             BrojPanela = 0;
             BrojTurbina = 0;
         }
@@ -29,7 +30,7 @@ namespace Solar_Panels_and_Wind_Generators
             {
                 return pocetnaProizvodnjaPanela * JacinaSunca / 100;
             }
-            
+
         }
 
         public int ProizvodnjaTurbina
@@ -40,6 +41,32 @@ namespace Solar_Panels_and_Wind_Generators
             }
 
         }
+        public void generisiJacinuSunca()
+        {
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
 
+            void OnTimedEvent(object source, ElapsedEventArgs e)
+            {
+                Random rand = new Random(Guid.NewGuid().GetHashCode());
+                JacinaSunca = rand.Next(0, 100);
+            }
+
+        }
+        public void generisiJacinuVetra()
+        {
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+
+            void OnTimedEvent(object source, ElapsedEventArgs e)
+            {
+                Random rand = new Random(Guid.NewGuid().GetHashCode());
+                JacinaVetra = rand.Next(0, 100);
+            }
+        }
     }
 }
