f022e83c6eaad6903547018c70f9c34c702d15ec
Milenko0 <116080511+Milenko0@users.noreply.github.com>
2023-01-09 14:58:58 +0100

    Add files via upload

---
+++ Power Distribution System/Test/ConsumerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Consumer;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class ConsumerTest
+    {
+        [Test]
+        [TestCase("Lampa")]
+        public void TestDodaj(string naziv)
+        {
+            Consumer.Consumer cm = new Consumer.Consumer();
+            List<Uredjaj> uredjaji = new List<Uredjaj>();
+
+            uredjaji.Add(new Uredjaj()
+            {
+                Naziv = "Lampa",
+                PotrosnjaEnergije = 40,
+                Ukljucen = true
+            });
+
+            Assert.AreEqual(uredjaji[0].Naziv, naziv);
+        }
+
+        [Test]
+        [TestCase("Lampa")]
+        public void TestUkloni(string naziv)
+        {
+            Consumer.Consumer cm = new Consumer.Consumer();
+            List<Uredjaj> uredjaji = new List<Uredjaj>();
+
+            uredjaji.Add(new Uredjaj()
+            {
+                Naziv = "Lampa",
+                PotrosnjaEnergije = 40,
+                Ukljucen = true
+            });
+
+            uredjaji.Remove(uredjaji[0]);
+
+            Assert.IsEmpty(uredjaji);
+        }
+    }
+}
